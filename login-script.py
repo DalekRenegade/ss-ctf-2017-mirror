d962a6e9c2bc177b0c83291b67d31d07fe171abb
Vishnu N <vishnunarang2000@gmail.com>
2017-04-12 19:32:13 -0700

    Updating login-script and adding files from server for inspection.

--- login-script.py
+++ login-script.py
@@ -2,6 +2,9 @@
 
 from ictf import iCTF
 import os
+from sys import argv
+
+script,user = argv
 
 # i=iCTF("http://35.161.233.76/")
 i=iCTF("http://52.34.158.221/")
@@ -22,6 +25,9 @@ print key_info['port']
 os.system("chmod 600 ctf_key")
 os.system("chmod 600 root_key")
 
-ssh_string = "ssh -i ctf_key ctf@{0} -p {1}".format(key_info['ip'],key_info['port'])
+if user == 'root':
+	ssh_string = "ssh -i root_key root@{0} -p {1}".format(key_info['ip'],key_info['port'])	
+else:
+	ssh_string = "ssh -i ctf_key ctf@{0} -p {1}".format(key_info['ip'],key_info['port'])
 
 os.system(ssh_string)
