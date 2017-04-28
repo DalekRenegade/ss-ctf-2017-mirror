276358e319927f76b32575cb939c2f07d216098a
Vishnu N <vishnunarang2000@gmail.com>
2017-04-28 16:17:53 -0700

    updating loging script and not pushing secret file

--- Finals/login.py
+++ Finals/login.py
@@ -10,12 +10,15 @@ argc = len(argv)
 if argc > 1:
 	user = argv[1] or "ctf"
 
-username = ""
-passwd = ""
 
 if username == "":
 	print "You forgot to copy credentials from the email..."
+	print "Create a file named 'secret.txt' in the finals directory and put first line as username and second line as passwd."
 	exit(0)
+
+with open("secret.txt", 'r') as f:
+	username = f.readline()
+	passwd = f.readline()
 
 i=iCTF("http://35.167.152.77/")
 
