5774ffeb7dd9064b34f8ab14a417b5b29e284491
namnayak <nnayak3@asu.edu>
2017-04-28 20:55:47 -0700

    collects all c files from the given directory and includes checks

--- includeLib.py
+++ includeLib.py
@@ -1,38 +1,44 @@
-import sys
+import sys,os
 
-filename = sys.argv[1]
+directory = sys.argv[1]
+files=[]
+for dirpath, dirnames, filenames in os.walk(directory):
+    for filename in [f for f in filenames if f.endswith(".c")]:
+        files.append(os.path.join(dirpath, filename))
+        
 executeCommands=['system','exec']
 flag=0
 startline="main("
 brace="{"
 check1="\nargv = init(argc, argv);\nif (checkShellCode(argc,argv)!=0 || checkPathVulnerability(arc,argv)!=0){\nreturn 1;\n}\n"
 check2="\nargv = init(argc, argv);\nif (checkShellCode(argc,argv)!=0 || checkPathVulnerability(arc,argv)!=0 || checkCommandInjection(argc,argv)!=0){\nreturn 1;\n}\n"
-with open(filename, 'r') as f1:
-	lines = f1.readlines()
+for filename in files:
+	with open(filename, 'r') as f1:
+		lines = f1.readlines()
 
-lines[1]=lines[1]+ "#include<cpatcher.h>\n#include<cpatchersys.h>\n"
-for i in range(0,len(lines)):
-	if executeCommands[0] in lines[i]:
-		flag =1
-		break
-	if executeCommands[1] in lines[i]:
-		flag =1
-		break
-
-for i in range(0,len(lines)):
-	if startline in lines[i]:
-		if flag == 1:
-			if brace in lines[i]:
-				lines[i]=lines[i]+check2
-			elif brace in lines[i+1]:
-				lines[i+1]=lines[i+1]+check2
+	lines[1]=lines[1]+ "#include \"cpatcher.h\"\n#include \"cpatchersys.h\"\n"
+	for i in range(0,len(lines)):
+		if executeCommands[0] in lines[i]:
+			flag =1
 			break
-		else:
-			if brace in lines[i]:
-				lines[i]=lines[i]+check1
-			elif brace in lines[i+1]:
-				lines[i+1]=lines[i+1]+check1
+		if executeCommands[1] in lines[i]:
+			flag =1
 			break
 
-with open(filename, 'w') as f2:
-	f2.writelines(lines)
+	for i in range(0,len(lines)):
+		if startline in lines[i]:
+			if flag == 1:
+				if brace in lines[i]:
+					lines[i]=lines[i]+check2
+				elif brace in lines[i+1]:
+					lines[i+1]=lines[i+1]+check2
+				break
+			else:
+				if brace in lines[i]:
+					lines[i]=lines[i]+check1
+				elif brace in lines[i+1]:
+					lines[i+1]=lines[i+1]+check1
+				break
+
+	with open(filename, 'w') as f2:
+		f2.writelines(lines)
