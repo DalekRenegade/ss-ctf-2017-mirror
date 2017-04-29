fae62ab171d48f53a88e9d0a0cd9aebd5918a077
Vishnu N <vishnunarang2000@gmail.com>
2017-04-28 17:45:13 -0700

    updating info file to give us info for services and teams.

---
+++ Finals/info.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+
+import os
+from ictf import iCTF
+from sys import argv
+
+argc = len(argv)
+
+with open("secret.txt", 'r') as f:
+	username = f.readline().strip()
+	passwd = f.readline().strip()
+
+if username == "":
+	print "You forgot to copy credentials from the email..."
+	print "Create a file named 'secret.txt' in the finals directory and put first line as username and second line as passwd."
+	exit(0)
+
+i = iCTF("http://35.167.152.77/")
+t = i.login(username,passwd)
+
+option = input("Enter one of these: \n1: services\n2: targets\n")
+
+if option == 1:
+	services = t.get_service_list();
+	for service in services:
+		print "*******"
+		print "name\t: {0}".format(service['service_name'])
+		print "state\t: {0}".format(service['state'])
+		print "port\t: {0}".format(service['port'])
+		print "description\t: {0}".format(service['description'])
+
+elif option == 2:
+	service = raw_input("Which service: ")
+	targets = t.get_targets(service)['targets']
+
+	for target in targets:
+		print "*********"
+		print "Hostname:\t{0}".format(target['hostname'])
+		print "PORT: \t\t{0}".format(target['port'])
+		print "flag_id: \t{0}".format(target['flag_id'])
+		print "team name: \t{0}".format(target['team_name'])
--- server-demo-files/wrapper.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /opt/ctf/sample_py/rw
-
-if [[ "i386" == "x86_64" ]] || [[ "x86_64" == "x86_64" ]] ; then
-  ../ro/sample_py 2>/dev/null
-else
-  qemu-x86_64 ../ro/sample_py 2>/dev/null
