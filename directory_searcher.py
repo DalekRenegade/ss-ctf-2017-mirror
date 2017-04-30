82b9699bd26f2f45fbd926bc54665bd0a8a846bc
vachan <vachan@ubuntu.ubuntu-domain>
2017-04-30 12:16:00 -0700

    Updated directory listener

--- directory_searcher.py
+++ directory_searcher.py
@@ -13,7 +13,22 @@ import re
 
 os.chdir(argv[1])
 for file in glob.glob("*.py"):
-	fp = open(file, "r")
+	with open(file, 'r') as f1:
+		lines = f1.readlines()
+		for lineNum, line in enumerate(lines):
+						
+			if 'os.environ' in line:
+				m=re.search(r"\[\'([A-Za-z0-9_]+)\'\]",line)
+				if m:
+					print("\t\t\t ----Information for attacking---- ")
+					print m.group(1)+'\tLine Number: '+str(lineNum)+"\tin file: "+str(file)					
+					lines[lineNum]=lines[lineNum].replace(m.group(1),str(os.getenv(m.group(1))))
+		
+	with open(file, 'w') as f1:
+		f1.writelines(lines)
+	
+
+"""	fp = open(file, "r")
 	for lineNum, line in enumerate(fp.readlines(),1):
 		if 'os.system' in line:
 			#print file
@@ -22,7 +37,7 @@ for file in glob.glob("*.py"):
 			#print file
 			m = re.search(r"\[\'([A-Za-z0-9_]+)\'\]",line)
 			if m:
-				print m.group(1)+'\tLine Number: '+str(lineNum)+"in file: "+str(file)
+				print m.group(1)+'\tLine Number: '+str(lineNum)+"\tin file: "+str(file)
 
 			fp.close()
-
+"""
