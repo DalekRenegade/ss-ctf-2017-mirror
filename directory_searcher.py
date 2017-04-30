68b04666b243664aee85293e9cca0c983bca7fbe
vachan <vachan@ubuntu.ubuntu-domain>
2017-04-29 17:54:57 -0700

    Updated directory-searcher.py with comments and printing file name along with line number of env var

--- directory_searcher.py
+++ directory_searcher.py
@@ -1,3 +1,10 @@
+""" 
+command to use it is:
+python directory_searcher.py <file_path>
+Version 1: You will get an output of all the environment variables being accessed by the 
+os.environ command and the line number it exists on in the particular file.
+
+"""
 import os
 import glob
 from sys import argv
@@ -15,7 +22,7 @@ for file in glob.glob("*.py"):
 			#print file
 			m = re.search(r"\[\'([A-Za-z0-9_]+)\'\]",line)
 			if m:
-				print m.group(1)+'\tLine Number: '+str(lineNum)
+				print m.group(1)+'\tLine Number: '+str(lineNum)+"in file: "+str(file)
 
 			fp.close()
 
