b0469ff929ec8dede7c6b9bff4c49a45a31caf44
Vishnu N <vishnunarang2000@gmail.com>
2017-05-01 16:46:58 -0700

    Changing script...

--- csanitizer_helper.sh
+++ csanitizer_helper.sh
@@ -1,5 +1,5 @@
 ls /bin > /var/restricted_cmd.txt
 chmod 444 /var/restricted_cmd.txt
-cp /home/team9/code/csanitizer.h /usr/include/csanitizer.h
+cp ~/code/csanitizer.h /usr/include/csanitizer.h
 chmod 644 /usr/include/csanitizer.h
-cp -a /opt/ctf/. /home/team9/ctf
+# cp -a /opt/ctf/. /home/team9/ctf
