772223efe31517fc321251f05ea0fae542bebda9
Komal <kpandhar@asu.edu>
2017-05-02 00:49:08 +0100

    line commented

--- xssFormInfo.py
+++ xssFormInfo.py
@@ -10,15 +10,15 @@ for root,dir,files in os.walk("/home"):
         if file.endswith(".php") or file.endswith(".html"):
             #print "found HTML file : " + file
 
-            if(file == 'testPhp.php' or file == 'test1.html' ):
-                input_variables[file] = []
-                page =open(os.path.join(root,file))
-                soup = BeautifulSoup(page.read())
-                inputs = soup.find_all("input")
-                for input in inputs:
-                    try:
-                        if input['name']:
-                            input_variables[file].append(input['name'])
-                    except:
-                        continue
+            #if(file == 'testPhp.php' or file == 'test1.html' ):
+            input_variables[file] = []
+            page =open(os.path.join(root,file))
+            soup = BeautifulSoup(page.read())
+            inputs = soup.find_all("input")
+            for input in inputs:
+               try:
+                  if input['name']:
+                       input_variables[file].append(input['name'])
+               except:
+                  continue
 print input_variables
